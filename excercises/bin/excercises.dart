8815a0b3595fe2fb9fc681dcb6f7a863fc903c7d
IonelaChiriac <ionela.s.chiriac@gmail.com>
2020-10-25 14:35:08 +0200

    second commit

--- excercises/bin/excercises.dart
+++
@@ -1,5 +0,0 @@
-import 'package:excercises/excercises.dart' as excercises;
-
-void main(List<String> arguments) {
-  print('Hello world: ${excercises.calculate()}!');
-}
---
+++ exercises/bin/exercises.dart
@@ -0,0 +1,5 @@
+import 'package:exercises/exercises.dart' as exercises;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exercises.calculate()}!');
+}
