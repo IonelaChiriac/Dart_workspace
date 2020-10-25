2bfaaa95d3f442efaf2e797c3d198dd811dbffa6
IonelaChiriac <ionela.s.chiriac@gmail.com>
2020-10-25 15:33:18 +0200

    24 oct commit

--- exercises/bin/exercises.dart
+++ exercises/bin/exercises.dart
@@ -2,5 +2,11 @@ import 'package:exercises/exercises.dart' as exercises;
 
 void main() {
   String name = 'Jo';
-  print("Hello, I'm, $name");
+  int age = 36;
+  double height = 1.60;
+
+  print("Hello, I'm, $name"); //$name string interpolation
+  print("My name has ${name.length} letters");
+  print("I'm $age years old");
+  print("I'm $height meters tall");
 }
