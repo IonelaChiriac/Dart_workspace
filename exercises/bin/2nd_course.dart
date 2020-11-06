ff298c4db729590bc644390b222f6a2f9d8a9ffe
IonelaChiriac <ionela.s.chiriac@gmail.com>
2020-11-06 22:01:51 +0200

    6 nov commit

---
+++ exercises/bin/2nd_course.dart
@@ -0,0 +1,50 @@
+import 'dart:ffi';
+
+main(List<String> arguments) {
+  //entry point for execution
+
+  ///variable = spot in memory or just a bucket to hold a value
+
+//Data types
+
+// String
+// Number (integers and doubles)
+// Boolean - 1 or 0, or true or false
+// lists
+// maps
+// ...
+
+//   var country;
+
+//   String name; //expicit
+
+//   name = "Jo";
+//   country = "Romania";
+//   country = 23;
+
+//   print(country);
+// }
+
+//numbers integers and doubles excercise
+
+//   int age = 13;
+//   double number = 23.30;
+
+//   print(number);
+// }
+
+//booleans excercise
+
+//   bool isTrue = true;
+//   bool isFalse = false;
+
+//   print(isTrue);
+// }
+
+//const and final exercise
+//const = we use this keyword when we want the value/variable to be a constant at compile-time
+//final = if we want the variable/value to always be constant = never change
+
+//   const pi = 3.14;
+//   pi = 23.89;
+// }
