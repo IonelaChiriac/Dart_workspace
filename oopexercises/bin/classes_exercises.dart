27e3c49e29529da333b5f788f59050e5431686c1
IonelaChiriac <ionela.s.chiriac@gmail.com>
2020-11-12 21:44:31 +0200

    12 nov classes commit

--- oopexercises/bin/classes_exercises.dart
+++ oopexercises/bin/classes_exercises.dart
@@ -2,6 +2,22 @@ class Microphone {
   String name;
   String color;
   int model = 2021; //already set
+
+//methods
+  void turnOn() {
+    print("$name is on!");
+  }
+
+  void turnOff() {
+    print("$name is turned off!");
+  }
+
+  bool isOn() => true;
+  int modelNumber() => model;
+
+  void setVolume() {
+    print("$name with color: $color volume is up!");
+  }
 }
 
 main(List<String> arguments) {
@@ -10,5 +26,12 @@ main(List<String> arguments) {
   mic.color = "Silver Gray";
   mic.model = 2020; // no need to set here again - it will overritte
 
-  print(mic.name); // .(dot) access operator
+  print(mic.model); // .(dot) access operator
+
+  mic.turnOn();
+  mic.setVolume();
+  mic.turnOff();
+
+  print(mic.isOn());
+  print(mic.modelNumber());
 }
