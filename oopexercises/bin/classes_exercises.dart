100f6648ac3be011aea9c9538a65ac8761d61737
IonelaChiriac <ionela.s.chiriac@gmail.com>
2020-11-13 07:18:40 +0200

    13 nov constructors setter and getters

--- oopexercises/bin/classes_exercises.dart
+++ oopexercises/bin/classes_exercises.dart
@@ -1,19 +1,43 @@
 class Microphone {
+  //Instance variables, member variables
+  //this = this object/class
   String name;
   String color;
-  int model = 2021; //already set
+  int model = 4536;
 
-//methods
+  //Syntactic sugar constructor
+  Microphone(this.name, this.color, this.model);
+
+// Named Constructor
+  Microphone.initialize() {
+    name = "Blue Yeti 2nd Edition";
+    model = 67;
+  }
+
+  String get getName => name; // getter
+  set setName(String value) => name = value; // setter
+
+  //constructor
+//   Microphone(String name, String color, int model) {
+//
+//       this.name = name;
+//       this.color = color;
+//       this.model = model;
+//   }
+
+  //Microphone(this.name, this.color, this.model);
+
   void turnOn() {
     print("$name is on!");
   }
 
+  bool isOn() => true;
+
+  int modelNumber() => model;
+
   void turnOff() {
     print("$name is turned off!");
   }
-
-  bool isOn() => true;
-  int modelNumber() => model;
 
   void setVolume() {
     print("$name with color: $color volume is up!");
@@ -21,17 +45,27 @@ class Microphone {
 }
 
 main(List<String> arguments) {
-  var mic = new Microphone(); //Instanciating an object = creating an object
-  mic.name = "Blue Yeti";
-  mic.color = "Silver Gray";
-  mic.model = 2020; // no need to set here again - it will overritte
+  var mic = new Microphone("Blue Yeti", "Silver gray",
+      1345); // we are crating the actual object of type mic
 
-  print(mic.model); // .(dot) access operator
+  mic.setName = "NewName";
 
-  mic.turnOn();
-  mic.setVolume();
-  mic.turnOff();
+  String name = "Paulo";
 
-  print(mic.isOn());
-  print(mic.modelNumber());
+  print(mic.getName);
+
+//  var micSecond = new Microphone.initialize(); // using the initialize named constructor
+//
+//  print(micSecond.model);
+//  print(micSecond.name);
+//
+// // print(mic.model); //. (dot) access operator
+//  mic.model = 8837647;
+//
+//  mic.turnOn();
+//  mic.setVolume();
+//  mic.turnOff();
+//
+//  print(mic.isOn());
+//  print(mic.modelNumber());
 }
